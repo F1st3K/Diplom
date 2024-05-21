e701e89d90c6da5626140461d5a596728defb612
F1st3K <N1k0s04k@gmail.com>
2024-05-21 23:54:08 +0300

    create full attendence view form

---
+++ AttendanceTracking/src/AttendanceTracking.View/Components/AditionMonthTable.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttendanceTracking.View.Components
+{
+    /// <summary>
+    /// Interaction logic for AditionMonthTable.xaml
+    /// </summary>
+    public partial class AditionMonthTable : UserControl
+    {
+        public class Value
+        {
+            public int Excused { get; private set; }
+            public int Unexcused { get; private set; }
+            public Value(int excused, int unxcused)
+            {
+                if (excused < 0)
+                    throw new ArgumentException("excused must be more zero");
+                if (unxcused < 0)
+                    throw new ArgumentException("uxcused must be more zero");
+                Excused = excused;
+                Unexcused = unxcused;
+            }
+        }
+
+        public AditionMonthTable(string month, IEnumerable<Value> attendenses)
+        {
+            InitializeComponent();
+            MonthName.Text = month;
+                
+                
+            AditionalTable.ItemsSource = attendenses
+                .Concat(new Value[] { new Value(
+                    attendenses.Sum(s => s.Excused),
+                    attendenses.Sum(s => s.Unexcused)) 
+                })
+                .Select(a => new
+                {
+                    Excused = a.Excused != 0 ? a.Excused.ToString() : "",
+                    Unexcused = a.Unexcused != 0 ? a.Unexcused.ToString() : "",
+                });
+        }
+
+        private void AditionalTable_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < AditionalTable.Items.Count; i++)
+            {
+                DataGridRow row = (DataGridRow)AditionalTable.ItemContainerGenerator.ContainerFromIndex(i);
+                {
+                    DataGridCell cell = AditionalTable.Columns[0].GetCellContent(row).Parent as DataGridCell;
+                    if (((TextBlock)cell.Content).Text != string.Empty)
+                    {
+                        Style style = new Style(typeof(DataGridCell));
+                        style.Setters.Add(new Setter(DataGridCell.BackgroundProperty, Brushes.CadetBlue));
+                        cell.Style = style;
+                    }
+                }
+                {
+                    DataGridCell cell = AditionalTable.Columns[1].GetCellContent(row).Parent as DataGridCell;
+                    if (((TextBlock)cell.Content).Text != string.Empty)
+                    {
+                        Style style = new Style(typeof(DataGridCell));
+                        style.Setters.Add(new Setter(DataGridCell.BackgroundProperty, Brushes.PaleVioletRed));
+                        cell.Style = style;
+                    }
+                }
+                if (i == AditionalTable.Items.Count - 1)
+                {
+                    {
+                        DataGridCell cell = AditionalTable.Columns[0].GetCellContent(row).Parent as DataGridCell;
+                        if (((TextBlock)cell.Content).Text != string.Empty)
+                        {
+                            Style style = new Style(typeof(DataGridCell));
+                            style.Setters.Add(new Setter(DataGridCell.BackgroundProperty, Brushes.Green));
+                            cell.Style = style;
+                        }
+                    }
+                    {
+                        DataGridCell cell = AditionalTable.Columns[1].GetCellContent(row).Parent as DataGridCell;
+                        if (((TextBlock)cell.Content).Text != string.Empty)
+                        {
+                            Style style = new Style(typeof(DataGridCell));
+                            style.Setters.Add(new Setter(DataGridCell.BackgroundProperty, Brushes.Red));
+                            cell.Style = style;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- AttendanceTracking/src/AttendanceTracking.View/Forms/AttendanceViewForm.xaml.cs
+++ AttendanceTracking/src/AttendanceTracking.View/Forms/AttendanceViewForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AttendanceTracking.View.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,107 @@ namespace AttendanceTracking.View.Forms
     /// </summary>
     public partial class AttendanceViewForm : Window
     {
-        public AttendanceViewForm()
+        private string[] russianMonths = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+        private Func<DateTime, IEnumerable<AditionMonthTable.Value>> _getAttendenceMonthQuery;
+
+        public class AttendenceMonth
+        {
+            public DateTime Month { get; private set; }
+            public IEnumerable<AditionMonthTable.Value> Attendence { get; private set; }
+
+            public AttendenceMonth(DateTime month, IEnumerable<AditionMonthTable.Value> attendence)
+            {
+                Month = month;
+                Attendence = attendence;
+            }
+        }
+        private List<AttendenceMonth> Months = new List<AttendenceMonth>();
+
+        public AttendanceViewForm(
+            string groupName,
+            IEnumerable<string> students,
+            Func<DateTime, IEnumerable<AditionMonthTable.Value>> getAttendenceMonthQuery)
         {
             InitializeComponent();
+            TextGroup.Text = groupName;
+            StudentsTable.ItemsSource = students
+                .Select((s, i) => new { Id = i + 1, FullName = s })
+                .Concat(new object[] { new { FullName = "Всего пропусков:" } });
+            _getAttendenceMonthQuery = getAttendenceMonthQuery;
+            ComboYear.ItemsSource = new string[] { 
+                DateTime.Now.Year.ToString(),
+                (DateTime.Now.Year - 1).ToString(),
+                (DateTime.Now.Year - 2).ToString(),
+                (DateTime.Now.Year - 3).ToString(),
+            };
+            ComboYear.SelectedIndex = 0;
+            ComboSemestr.SelectionChanged += InitMonthTables;
+            ComboYear.SelectionChanged += InitMonthTables;
+            InitMonthTables(this, null);
         }
+
+        private void InitMonthTables(object sender, RoutedEventArgs e)
+        {
+            Months.Clear();
+            if (ComboSemestr.SelectedIndex == 0 || ComboSemestr.SelectedIndex == 1)
+            {
+                var sep = new DateTime(int.Parse(ComboYear.Text), 9, 1);
+                Months.Add(new AttendenceMonth(sep, _getAttendenceMonthQuery(sep).ToArray()));
+                var oct = new DateTime(int.Parse(ComboYear.Text), 10, 1);
+                Months.Add(new AttendenceMonth(oct, _getAttendenceMonthQuery(oct).ToArray()));
+                var nov = new DateTime(int.Parse(ComboYear.Text), 11, 1);
+                Months.Add(new AttendenceMonth(nov, _getAttendenceMonthQuery(nov).ToArray()));
+                var dec = new DateTime(int.Parse(ComboYear.Text), 12, 1);
+                Months.Add(new AttendenceMonth(dec, _getAttendenceMonthQuery(dec).ToArray()));
+            }
+            if (ComboSemestr.SelectedIndex == 0 || ComboSemestr.SelectedIndex == 2)
+            {
+                var yan = new DateTime(int.Parse(ComboYear.Text) + 1, 1, 1);
+                Months.Add(new AttendenceMonth(yan, _getAttendenceMonthQuery(yan).ToArray()));
+                var feb = new DateTime(int.Parse(ComboYear.Text) + 1, 2, 1);
+                Months.Add(new AttendenceMonth(feb, _getAttendenceMonthQuery(feb).ToArray()));
+                var mar = new DateTime(int.Parse(ComboYear.Text) + 1, 3, 1);
+                Months.Add(new AttendenceMonth(mar, _getAttendenceMonthQuery(mar).ToArray()));
+                var may = new DateTime(int.Parse(ComboYear.Text) + 1, 5, 1);
+                Months.Add(new AttendenceMonth(may, _getAttendenceMonthQuery(may).ToArray()));
+                var jun = new DateTime(int.Parse(ComboYear.Text) + 1, 6, 1);
+                Months.Add(new AttendenceMonth(jun, _getAttendenceMonthQuery(jun).ToArray()));
+            }
+
+            var enumerableAttendence = Months.First().Attendence
+                .Select((_, i) =>
+                {
+                    var excused = 0;
+                    var unexcused = 0;
+                    foreach (var m in Months.Select(m => m.Attendence.ToArray()[i]))
+                    {
+                        excused += m.Excused;
+                        unexcused += m.Unexcused;
+                    }
+                    return new AditionMonthTable.Value(excused, unexcused);
+                });
+
+            AditionalTable.ItemsSource = enumerableAttendence
+                .Select(a => new
+                {
+                    Sum = a.Excused + a.Unexcused,
+                    Excused = a.Excused != 0 ? a.Excused.ToString() : "",
+                    Unexcused = a.Unexcused != 0 ? a.Unexcused.ToString() : "",
+                })
+                .Concat(new object[] { new
+                    {
+                        Sum = enumerableAttendence.Sum(s => s.Excused) + enumerableAttendence.Sum(s => s.Unexcused),
+                        Excused = enumerableAttendence.Sum(s => s.Excused),
+                        Unexcused = enumerableAttendence.Sum(s => s.Unexcused)
+                    }
+                });
+
+            MonthDataGrid.Children.Clear();
+            foreach (var month in Months)
+                MonthDataGrid.Children.Add(new AditionMonthTable(russianMonths[month.Month.Month - 1], month.Attendence));
+
+        }
+
+
     }
 }
