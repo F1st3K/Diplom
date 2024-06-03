baba691e15979d80ffec6d141bb8683c0699d63c
F1st3K <N1k0s04k@gmail.com>
2024-06-03 13:09:51 +0300

    create Account forms

---
+++ AttendanceTracking/src/AttendanceTracking.View/Forms/AccountsCreateForm.xaml.cs
@@ -0,0 +1,76 @@
+﻿using AttendanceTracking.View.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AttendanceTracking.View.Forms
+{
+    /// <summary>
+    /// Interaction logic for AccountsCreateForm.xaml
+    /// </summary>
+    public partial class AccountsCreateForm : Window
+    {
+        public delegate void CreateHandler(AccountsViewForm.Account people);
+        public event CreateHandler Created;
+
+        private IEnumerable<AccountsViewForm.People> _peoples;
+        private AccountsViewForm.People _selectedPeople;
+
+        private HashService _hasher = new HashService();
+
+        private SearchService _searcher = new SearchService();
+
+        public AccountsCreateForm(IEnumerable<AccountsViewForm.People> peoples)
+        {
+            _peoples = peoples;
+
+            InitializeComponent();
+            Created += p => Close();
+            People.ItemsSource = _peoples.Select(p => toStr(p));
+        }
+
+        private void People_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (People.SelectedIndex < 0
+                || People.SelectedIndex >= _peoples.Count())
+                return;
+            _selectedPeople = _peoples.ElementAt(People.SelectedIndex);
+            Fullname.Text = _selectedPeople.FullName;
+            TextRoles.Text = _selectedPeople.TextRoles;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var peoples = _peoples.ToList();
+            peoples.Sort((x, y) =>
+            {
+                var distX = _searcher.GetLevenshteinDistance(toStr(x), SearchBox.Text);
+                var distY = _searcher.GetLevenshteinDistance(toStr(y), SearchBox.Text);
+                return distX.CompareTo(distY);
+            });
+            _peoples = peoples;
+            People.ItemsSource = _peoples.Select(p => toStr(p));
+        }
+        string toStr(AccountsViewForm.People p) => $"{p.FullName} ({p.TextRoles})";
+
+        private void CreateClick(object sender, RoutedEventArgs e)
+        {
+            Created?.Invoke(new AccountsViewForm.Account(_selectedPeople, Login.Text, _hasher.Hash(Password.Text), false));
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ AttendanceTracking/src/AttendanceTracking.View/Forms/AccountsViewForm.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AttendanceTracking.View.Forms
+{
+    /// <summary>
+    /// Interaction logic for AccountsViewForm.xaml
+    /// </summary>
+    public partial class AccountsViewForm : Window
+    {
+        public class People
+        {
+            public int Id { get; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Patronomic { get; set; }
+            public string FullName => $"{LastName} {FirstName} {Patronomic}";
+
+
+            public IEnumerable<string> Roles { get; }
+            public string TextRoles => string.Join(", ", Roles);
+
+
+            public People(int id, string firstName, string lastName, string patronomic, IEnumerable<string> roles)
+            {
+                Id = id;
+
+                FirstName = firstName;
+                LastName = lastName;
+                Patronomic = patronomic;
+
+                Roles = roles;
+            }
+        }
+
+        public class Account
+        {
+            public string Login { get; set; }
+
+            public bool IsActive { get; set; }
+            public string Status => IsActive ? "Активирована" : "Не активирована";
+
+            public string Hash { get; set; }
+
+            public People People { get; }
+            public int Id => People.Id;
+            public string FullName => People.FullName;
+            public string TextRoles => People.TextRoles;
+
+
+
+            public Account(People people, string login, string hash, bool isActive)
+            {
+                People = people;
+
+                Login = login;
+                IsActive = isActive;
+                Hash = hash;
+            }
+        }
+
+        
+
+        private List<Account> _accounts;
+
+        private Action<Account> _editCommand;
+        private Action<Account> _deleteComand;
+        private Func<Account, Account> _createCommand;
+        private Func<IEnumerable<People>> _getPeoplesQuery;
+
+        public AccountsViewForm(
+            IEnumerable<Account> accounts,
+            Action<Account> editAccount,
+            Action<Account> deleteAccount,
+            Func<Account, Account> createAccount,
+            Func<IEnumerable<People>> getPeoples
+            )
+        {
+            _accounts = accounts.ToList();
+            _editCommand = editAccount;
+            _deleteComand = deleteAccount;
+            _createCommand = createAccount;
+            _getPeoplesQuery = getPeoples;
+
+            InitializeComponent();
+
+            StudentsTable.ItemsSource = _accounts;
+            
+        }
+
+        private void EditClick(object sender, RoutedEventArgs e)
+        {
+            var s = (Button)sender;
+            var account = (Account)s.DataContext;
+
+            var editForm = new AccountsEditForm(account);
+            Hide();
+            editForm.Show();
+
+            editForm.Closed += (se, ev) => Show();
+            editForm.Edited += a =>
+            {
+                _editCommand?.Invoke(a);
+                StudentsTable.ItemsSource = _accounts.Select(p => p);
+                StudentsTable.Focus();
+            };
+        }
+
+        private void RemoveClick(object sender, RoutedEventArgs e)
+        {
+            var s = (Button)sender;
+            var account = (Account)s.DataContext;
+
+            var result = MessageBox.Show($"Вы точно хотите удалить учетную запись для {account.FullName}?", "Внимание", MessageBoxButton.YesNo);
+            
+            if (result == MessageBoxResult.Yes)
+            {
+                _accounts.Remove(account);
+                StudentsTable.ItemsSource = _accounts.Select(p => p);
+
+                _deleteComand?.Invoke(account);
+            }
+        }
+
+        private void CreateClick(object sender, RoutedEventArgs e)
+        {
+            var createForm = new AccountsCreateForm(_getPeoplesQuery?.Invoke()
+                .Where(p => _accounts.FindAll(a => a.People.Id == p.Id).Count == 0));
+            Hide();
+            createForm.Show();
+
+            createForm.Closed += (s, ev) => Show();
+            createForm.Created += account =>
+            {
+                account = _createCommand?.Invoke(account);
+
+                _accounts.Insert(0, account);
+                StudentsTable.ItemsSource = _accounts.Select(p => p);
+                StudentsTable.SelectedIndex = 0;
+                StudentsTable.Focus();
+            };
+
+        }
+    }
+
+    
+}
